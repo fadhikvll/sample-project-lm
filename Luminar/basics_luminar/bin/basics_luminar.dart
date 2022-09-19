d6fa4a5e68acc0aee86e0ea234134b77d54c9ccd
fadhikvl@gmail.com <fadhikvl@gmail.com>
2022-09-19 16:26:32 +0530

    commit

---
+++ Luminar/basics_luminar/bin/basics_luminar.dart
@@ -0,0 +1,5 @@
+import 'package:basics_luminar/basics_luminar.dart' as basics_luminar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basics_luminar.calculate()}!');
+}
---
+++ Luminar/basics_luminar/test/basics_luminar_test.dart
@@ -0,0 +1,8 @@
+import 'package:basics_luminar/basics_luminar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
