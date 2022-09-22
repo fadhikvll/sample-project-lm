8085fa7c181d7d6b090ea05224d1f39153dd5237
fadhikvl@gmail.com <fadhikvl@gmail.com>
2022-09-22 16:49:40 +0530

    commit

---
+++ myproject2/bin/myproject2.dart
@@ -0,0 +1,5 @@
+import 'package:myproject2/myproject2.dart' as myproject2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${myproject2.calculate()}!');
+}
---
+++ myproject2/test/myproject2_test.dart
@@ -0,0 +1,8 @@
+import 'package:myproject2/myproject2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
